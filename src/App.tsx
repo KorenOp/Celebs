1da6d60267b556a14459252311a492cb6c9f9943
Dani <dani@innovi.co.il>
2023-07-27 20:41:41 +0300

    start use

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MockCelebs from './assets/MockCelebs.json'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(MockCelebs)
+
 
   return (
     <>
