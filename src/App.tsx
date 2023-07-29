3648bf9492a1e4fdcf834b67f24be60a7f3f2ad1
Dani <dani@innovi.co.il>
2023-07-29 21:55:27 +0300

    finish styleing

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MockCelebs from './assets/MockCelebs.json'
+import React from 'react';
+import './App.css';
+import MockCelebs from './assets/MockCelebs.json';
+import RecipeReviewCard from './Card/Card';
 
-
+interface Person {
+  name: string;
+  birthDate: string;
+  gender: string;
+  role: string;
+  image: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(MockCelebs)
-
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-holder">
+        <div className='main-headline'> Celebs</div>
+        <div className='card-holder'>
+          {MockCelebs.map((celeb, index) => (
+            <RecipeReviewCard
+              key={index}
+              name={celeb.name}
+              birthDate={celeb.birthDate}
+              gender={celeb.gender}
+              role={celeb.role}
+              image={celeb.image}
+            />
+          ))}
+        </div>
       </div>
-      <h1>kopren is gevet</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
